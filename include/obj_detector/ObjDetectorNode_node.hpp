a5313d13172ead94a3541a64935f90c4be33bcdf
Andrew Ealovega <Andrew@Ealovega.dev>
2023-10-10 20:32:00 -0400

    Sync image topics

--- include/obj_detector/ObjDetectorNode_node.hpp
+++ include/obj_detector/ObjDetectorNode_node.hpp
@@ -1,17 +1,30 @@
 #pragma once
 
+#include <image_transport/image_transport.hpp>
+#include <image_transport/subscriber_filter.hpp>
+
+#include "geometry_msgs/msg/point.hpp"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image> MySyncPolicy;
 
 class ObjDetectorNode : public rclcpp::Node {
 private:
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub;
+    // To sync rgb and depth images from compressed streams
+    image_transport::SubscriberFilter rgb_syncer;
+    image_transport::SubscriberFilter depth_syncer;
+    message_filters::Synchronizer<MySyncPolicy> sync;
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub;
+    rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr point_pub;
 
 public:
     ObjDetectorNode(const rclcpp::NodeOptions& options);
 
-    /// subscriber callback
-    void sub_cb(std_msgs::msg::String::SharedPtr msg);
+    /// Synced images callback
+    void process_image(const sensor_msgs::msg::Image::ConstSharedPtr& rgb,
+                       const sensor_msgs::msg::Image::ConstSharedPtr& depth);
 };
--- src/ObjDetectorNode_node.cpp
+++ src/ObjDetectorNode_node.cpp
@@ -1,27 +1,25 @@
 #include "obj_detector/ObjDetectorNode_node.hpp"
 
+#include "functional"
+#include "opencv2/opencv.hpp"
+
 // For _1
 using namespace std::placeholders;
 
-ObjDetectorNode::ObjDetectorNode(const rclcpp::NodeOptions& options) : Node("ObjDetectorNode", options) {
-    // Parameters
-    float x = this->declare_parameter<float>("foo", -10.0);
-
-    // Pub Sub
-    this->sub =
-        this->create_subscription<std_msgs::msg::String>("/str", 1, std::bind(&ObjDetectorNode::sub_cb, this, _1));
-    this->pub = this->create_publisher<std_msgs::msg::String>("/run_folder", 1);
-
-    // Log a sample log
-    RCLCPP_INFO(this->get_logger(), "You passed %f", x);
-
-    // Send a sample message
-    std_msgs::msg::String msg{};
-    msg.data = std::string{"Hello World!"};
-    pub->publish(msg);
+ObjDetectorNode::ObjDetectorNode(const rclcpp::NodeOptions& options)
+    : Node("ObjDetectorNode", options),
+      // Sync the rgb and depth images, from compressed streams
+      rgb_syncer(this, "/camera/mid/rgb", "compressed"),
+      depth_syncer(this, "/camera/mid/depth", "compressed"),
+      sync(MySyncPolicy(10), rgb_syncer, depth_syncer) {
+    // Register callback for synced images
+    this->sync.registerCallback(std::bind(&ObjDetectorNode::process_image, this, _1, _2));
+    // Output detected objects
+    this->point_pub = this->create_publisher<geometry_msgs::msg::Point>("/object_poses", 5);
 }
 
-void ObjDetectorNode::sub_cb(const std_msgs::msg::String::SharedPtr msg) {
-    // Echo message
-    this->pub->publish(*msg);
+void ObjDetectorNode::process_image(const sensor_msgs::msg::Image::ConstSharedPtr& rgb,
+                                    const sensor_msgs::msg::Image::ConstSharedPtr& depth) {
+    RCLCPP_INFO(this->get_logger(), "Got image!");
+    //TODO detect the things
 }
