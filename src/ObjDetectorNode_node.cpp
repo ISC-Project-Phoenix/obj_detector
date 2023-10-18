4c8b3bc2fe71ed7603dac3796bdf317b895b7506
Andrew Ealovega <Andrew@Ealovega.dev>
2023-10-18 00:39:13 -0400

    Fix cones being blue in sim

--- src/ObjDetectorNode_node.cpp
+++ src/ObjDetectorNode_node.cpp
@@ -6,7 +6,7 @@
 // For _1
 using namespace std::placeholders;
 
-ObjDetectorNode::ObjDetectorNode(const rclcpp::NodeOptions &options) : Node("ObjDetectorNode", options) {
+ObjDetectorNode::ObjDetectorNode(const rclcpp::NodeOptions& options) : Node("ObjDetectorNode", options) {
     // Either "compressed" or "raw"
     std::string transport_type = this->declare_parameter("transport_type", "raw");
     this->debug = this->declare_parameter<bool>("debug", true);
@@ -17,23 +17,24 @@ ObjDetectorNode::ObjDetectorNode(const rclcpp::NodeOptions &options) : Node("Obj
     this->rgb_syncer = std::make_unique<image_transport::SubscriberFilter>(this, "/camera/mid/rgb", transport_type);
     this->depth_syncer = std::make_unique<image_transport::SubscriberFilter>(this, "/camera/mid/depth", "raw");
     this->sync =
-            std::make_unique<message_filters::Synchronizer<MySyncPolicy>>(MySyncPolicy(10), *rgb_syncer, *depth_syncer);
+        std::make_unique<message_filters::Synchronizer<MySyncPolicy>>(MySyncPolicy(10), *rgb_syncer, *depth_syncer);
 
     // Register callback for synced images
     this->sync->registerCallback(std::bind(&ObjDetectorNode::process_image, this, _1, _2));
 
     // Copy rgb camera info directly into model
     this->rgb_info_sub = this->create_subscription<sensor_msgs::msg::CameraInfo>(
-            "/camera/mid/rgb/camera_info", 1,
-            [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr ci) { this->rgb_model.fromCameraInfo(ci); });
+        "/camera/mid/rgb/camera_info", 1,
+        [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr ci) { this->rgb_model.fromCameraInfo(ci); });
 
     // Output detected objects
     this->point_pub = this->create_publisher<geometry_msgs::msg::PoseArray>("/object_poses", 5);
 }
 
-void ObjDetectorNode::process_image(const sensor_msgs::msg::Image::ConstSharedPtr &rgb,
-                                    const sensor_msgs::msg::Image::ConstSharedPtr &depth) {
-    auto cv_rgb = cv_bridge::toCvShare(rgb);
+void ObjDetectorNode::process_image(const sensor_msgs::msg::Image::ConstSharedPtr& rgb,
+                                    const sensor_msgs::msg::Image::ConstSharedPtr& depth) {
+    // Read as bgr8 to avoid cones being blue in sim
+    auto cv_rgb = cv_bridge::toCvShare(rgb, "bgr8");
     auto rgb_mat = cv_rgb->image;
 
     // First detect location of objects in pixel space
@@ -44,7 +45,7 @@ void ObjDetectorNode::process_image(const sensor_msgs::msg::Image::ConstSharedPt
     //this->point_pub->publish(poses);
 }
 
-std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat &rgb) {
+std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb) {
     cv::Mat mat;
     cv::cvtColor(rgb, mat, cv::COLOR_BGR2HSV);  // Changes image to HSV (Hue, Sat, value)
 
@@ -57,13 +58,13 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat &rgb) {
     cv::inRange(mat, lowerb, upperb, mask);
 
     // Find contours (Boundary lines of each masked area)
-    std::vector <std::vector<cv::Point>> contours;
-    std::vector <cv::Vec4i> hierarchy;
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
     cv::findContours(mask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
     // Code to calculate and annotate centroids (Center of each masked area)
-    std::vector <cv::Point2d> centers;
-    for (auto &contour: contours) {
+    std::vector<cv::Point2d> centers;
+    for (auto& contour : contours) {
         // Calculate moments for each contour
         cv::Moments M = cv::moments(contour);
 
@@ -73,18 +74,17 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat &rgb) {
 
             centers.push_back(center);
         }
-
     }
 
     // Show debug windows
     if (this->debug) {
         cv::Mat dbg;
         rgb.copyTo(dbg);
-        for (auto &center: centers) {
+        for (auto& center : centers) {
             cv::circle(dbg, center, 5, cv::Scalar(255, 0, 0), -1);
         }
 
-        cv::imshow("mask", mask);  // Shows image of mask
+        cv::imshow("mask", mask);    // Shows image of mask
         cv::imshow("centers", dbg);  // Shows image of mask
         cv::pollKey();               // Lets you see the images above.
     }
@@ -92,7 +92,7 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat &rgb) {
     return centers;
 }
 
-geometry_msgs::msg::PoseArray ObjDetectorNode::project_to_world(const std::vector<cv::Point2d> &object_locations,
-                                                                const sensor_msgs::msg::Image::ConstSharedPtr &depth) {
+geometry_msgs::msg::PoseArray ObjDetectorNode::project_to_world(const std::vector<cv::Point2d>& object_locations,
+                                                                const sensor_msgs::msg::Image::ConstSharedPtr& depth) {
     //TODO project
 }
