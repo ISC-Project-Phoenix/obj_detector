0a2f64d112ad71fcaeb7759481270fe00b5d33d3
Andrew Ealovega <Andrew@Ealovega.dev>
2023-11-02 22:29:06 -0400

    Format

--- src/ObjDetectorNode_node.cpp
+++ src/ObjDetectorNode_node.cpp
@@ -70,14 +70,12 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
     // Copy to gpu
     auto rgb = rgb_mat.getUMat(cv::ACCESS_READ, cv::USAGE_ALLOCATE_SHARED_MEMORY);
 
-
     // Define gamma and create a lookup table
     //gamma will brighten shadows on image
-    double gamma = 0.7; // Value < 1 'Image brightens shadows. Value > 1 Image darkens shadows
+    double gamma = 0.7;  // Value < 1 'Image brightens shadows. Value > 1 Image darkens shadows
     cv::Mat lookUpTable(1, 256, CV_8U);
-    uchar *p = lookUpTable.ptr();
-    for (int i = 0; i < 256; ++i)
-        p[i] = cv::saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
+    uchar* p = lookUpTable.ptr();
+    for (int i = 0; i < 256; ++i) p[i] = cv::saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
 
     // Apply gamma correction
     cv::UMat mat_gamma_corrected;
@@ -91,11 +89,9 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
     cv::Scalar upperb = cv::Scalar(25, 255, 255);
     cv::Scalar lowerb = cv::Scalar(1, 120, 130);
 
-
     //Applying the above bounds to the gamma_image to create mask
     cv::UMat mask;
     cv::inRange(hsv, lowerb, upperb, mask);
-
 
     // Define the structuring element for the morphological operations (Required for dilation operation)
     int morphSize = 3;  // change this as per requirement
@@ -105,7 +101,6 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
     // Perform dilation
     cv::UMat mask_dilated;
     cv::dilate(mask, mask_dilated, element);
-
 
     // Find contours (Boundary lines of each masked area)
     std::vector<std::vector<cv::Point>> contours;
@@ -122,7 +117,7 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
         double areaThreshold = 1500.0 / (1280 * 720);
 
         // Checks if Masked Object is greater than areaThreshold.
-        if (M.m00 > areaThreshold * (rgb.rows * rgb.cols)) //M.m00 = total number of white pixels in contour.
+        if (M.m00 > areaThreshold * (rgb.rows * rgb.cols))  //M.m00 = total number of white pixels in contour.
         {
             //Uses moment values to find the center pixel (x,y)
             cv::Point2f center(M.m10 / M.m00, M.m01 / M.m00);
@@ -131,7 +126,7 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
             cv::Rect boundingRect = cv::boundingRect(contour);
 
             // Calculate the aspect ratio of the bounding rectangle
-            double aspect_ratio = (double) boundingRect.width / boundingRect.height;
+            double aspect_ratio = (double)boundingRect.width / boundingRect.height;
 
             // Calculate the ratio of the contour area to its arc length
             double area_perimeter_ratio = cv::contourArea(contour) / cv::arcLength(contour, true);
@@ -140,7 +135,6 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
             //CV_TEAM - Calculate specific values for the type of cones we will use, these values below are general TODO
             if (aspect_ratio > 0.5 && aspect_ratio < 1.5 && area_perimeter_ratio > 0.20) {
                 centers.push_back(center);
-
             }
         }
     }
@@ -149,8 +143,8 @@ std::vector<cv::Point2d> ObjDetectorNode::detect_objects(const cv::Mat& rgb_mat)
     if (this->debug) {
         cv::UMat dbg;
         mat_gamma_corrected.copyTo(dbg);
-        for (auto &center: centers) {
-            cv::circle(dbg, center, 5, cv::Scalar(255, 0, 0), -1); //Draws blue circle at centers
+        for (auto& center : centers) {
+            cv::circle(dbg, center, 5, cv::Scalar(255, 0, 0), -1);  //Draws blue circle at centers
         }
 
         cv::imshow("mask", mask);    // Shows image of mask
@@ -180,7 +174,7 @@ geometry_msgs::msg::PoseArray ObjDetectorNode::project_to_world(const std::vecto
         // Project pixel to camera space
         auto ray = this->rgb_model.projectPixelTo3dRay(center);
         // The oak-d uses shorts in mm, sim uses f32 in m
-        float dist = depth.type() == 2 ? (float) depth.at<short>(center) / 1000 : depth.at<float>(center);
+        float dist = depth.type() == 2 ? (float)depth.at<short>(center) / 1000 : depth.at<float>(center);
 
         // If depth unavailable, then skip
         if (dist == INFINITY || dist >= 10 || dist <= 0) {
