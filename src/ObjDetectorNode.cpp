32c1dfc88a367b5e9edf083e7d2a1581bee129d5
Andrew Ealovega <Andrew@Ealovega.dev>
2023-11-05 00:53:36 -0400

    Fix formatting

--- src/ObjDetectorNode.cpp
+++ src/ObjDetectorNode.cpp
@@ -3,21 +3,20 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char ** argv)
-{
-  //This code below was taken from python, but I want to code it in c++
+int main(int argc, char** argv) {
+    //This code below was taken from python, but I want to code it in c++
 
-  // Setup runtime
-  rclcpp::init(argc, argv);
-  rclcpp::executors::MultiThreadedExecutor exec;
-  rclcpp::NodeOptions options;
+    // Setup runtime
+    rclcpp::init(argc, argv);
+    rclcpp::executors::MultiThreadedExecutor exec;
+    rclcpp::NodeOptions options;
 
-  // Add nodes to executor
-  auto node = std::make_shared<ObjDetectorNode>(options);
-  exec.add_node(node);
+    // Add nodes to executor
+    auto node = std::make_shared<ObjDetectorNode>(options);
+    exec.add_node(node);
 
-  // Run
-  exec.spin();
-  rclcpp::shutdown();
-  return 0;
+    // Run
+    exec.spin();
+    rclcpp::shutdown();
+    return 0;
 }
--- tests/unit.cpp
+++ tests/unit.cpp
@@ -6,13 +6,12 @@
 
 TEST(ObjDetectorNode, Test1) {}
 
-int main(int argc, char ** argv)
-{
-  rclcpp::init(0, nullptr);
+int main(int argc, char** argv) {
+    rclcpp::init(0, nullptr);
 
-  ::testing::InitGoogleTest(&argc, argv);
-  auto res = RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    auto res = RUN_ALL_TESTS();
 
-  rclcpp::shutdown();
-  return res;
+    rclcpp::shutdown();
+    return res;
 }
