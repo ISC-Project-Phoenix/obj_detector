29ecbbf771699f1e5c7eb01c2f6c5dd30b2948a5
Jared <jwens@umich.edu>
2024-01-17 14:22:03 -0500

    Some Changes

--- include/obj_detector/DetectionStrategy.hpp
+++ include/obj_detector/DetectionStrategy.hpp
@@ -4,10 +4,23 @@
 
 #pragma once
 
+struct ObjDetectorParams{
+    // TODO param that changes detect type?
+};
+
 class DetectionStrategy {
 public:
-
     // TODO      This is probably wrong below
-    virtual std::vector<detect> detect_object_centers(const cv::Mat& rgb_Mat);
+    virtual DetectOption detect(const cv::Mat& rgb_Mat) = 0;
+    virtual ~IDetectionClassifier() = default;
+};
 
+class temp : public DetectionStrategy{
+public:
+    DetectionOption detect(const cv::Mat& rgb_Mat) override;
+
+protected:
+private:
+
+    // TODO Something here
 };
